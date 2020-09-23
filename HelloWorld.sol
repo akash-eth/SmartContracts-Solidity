df5d1a9bbcd059ae71668c0fc439ffe63d6f58bc
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-09-23 15:16:08 +0530

    hello world

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unidentified
+
+pragma solidity >=0.5.0;
+
+contract Helloworld {
+    function hello() pure public returns(string memory) {
+        return "hello world";
+    }
+}
