7ac33e160fb8ce93f4adea9553aa6197760ac3a4
Akash Bhardwaj <48449366+akashbhardwaj04@users.noreply.github.com>
2020-10-19 23:10:33 +0530

    Updated contract version and struct

--- DogoSpace/main.sol
+++ DogoSpace/main.sol
@@ -1,11 +1,12 @@
 //SPDX-License-Identifier: Unidentified
 
-pragma solidity <=0.5.0;
+pragma solidity <=0.4.20;
 
 contract dogoSpace{
-    struct candidates{
-        public int count;
-        string name;  
-    }
+    uint8 planetModulus;
+    struct doggo {
+    
+}
+  
     
 }
