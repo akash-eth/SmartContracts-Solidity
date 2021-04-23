5d5917ee549578133677bd0120c2b0712fe4b98f
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-04-23 19:03:04 +0530

    dApp 2

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: Unidentified
 
-pragma solidity >=0.5.0;
+pragma solidity >= 0.5.0;
 
-contract Helloworld {
-    function hello() pure public returns(string memory) {
-        return "hello world";
+contract HelloWorld {
+    
+    function sayHello() pure public returns(string memory) {
+        return 'Hello World';
     }
+    
 }
