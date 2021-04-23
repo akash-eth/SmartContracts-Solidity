b0cc5185d0a4745282bdb774ead375cd10ba4ada
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-04-23 19:06:19 +0530

    dApp 5

--- CRUDcontract.sol
+++ CRUDcontract.sol
@@ -1,45 +1,55 @@
 // SPDX-License-Identifier: Undefined
 
-pragma solidity ^0.7.0;
+pragma solidity >= 0.5.0;
 
-contract crud {
-
+contract CRUD {
+    
     struct User {
         uint id;
         string name;
     }
+    
 
     User[] public users;
-    uint public nextId;
-
-    function create(string memory name) public {
-        users.push(User(nextId, name));
-        nextId++;
+    
+    uint public nextId = 1;
+    
+    function create(string memory _name) public {
+        users.push(User(nextId, _name));
     }
-
-    function read(uint id) public view returns(uint, string memory) {
-        uint i = find(id);
+    
+    function read(uint _id) view public returns(uint, string memory) {
+        // now we have get user info based on IDs. So we'll need to implement loops
+        // for(uint i; i < users.length; i++) {
+        //     if(users[i].id == _id) {
+        //         return (users[i].id, users[i].name);
+        //     }
+        // }
+        uint i = findUser(_id);
         return (users[i].id, users[i].name);
-
     }
-
-    function update(uint id, string memory name) public {
-        uint i = find(id);
-        users[i].name = name;
+    
+    function update(uint _id, string memory _updatedName) public {
+        // for(uint i; i < users.length; i++) {
+        //     if(users[i].id == _id) {
+        //         users[i].name = _updatedName;
+        //     }
+        // }
+        uint i = findUser(_id);
+        users[i].name = _updatedName;
     }
-
-    function del(uint id) public {
-        uint i = find(id);
+    
+    function deleteUser(uint _id) public {
+        uint i = findUser(_id);
         delete users[i];
     }
-
-
-    function find(uint id) view internal returns(uint){
-        for (uint i; i < users.length; i++) {
-            if (users[i].id == id) {
+    
+    function findUser(uint _id) view public returns(uint) {
+        for(uint i; i < users.length; i++) {
+            if(users[i].id == _id) {
                 return i;
             }
         }
-        revert("Requested user does not exist");
+        revert ('Sorry the user does not exist !!');
     }
 }
