74b48825974e68cfbad4720d58cfd8c97924a812
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-05-24 15:40:35 +0530

    Initiated mock ZRX

---
+++ De-Exchange/Zrx.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:minutes
+
+pragma solidity ^0.8.0;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Zrx is ERC20 {
+    
+    constructor() ERC20("ZRX", "0x Project") {}
+    
+}
