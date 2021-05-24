d90e479233c83ec708382bbb7c63665a3e2b73bb
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-05-24 15:40:02 +0530

    Initiated mock REP

--- De-Exchange/Rep.sol
+++ De-Exchange/Rep.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.0;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
-contract Dai is ERC20 {
+contract Rep is ERC20 {
     
-    constructor() ERC20("DAI", "Dai Stablecoin") {}
+    constructor() ERC20("Rep", "Augur") {}
     
 }
-
