46b128cb757258a524073badc0fe6c7cdd8e29ce
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-05-24 15:41:09 +0530

    Initiated mock BAT

---
+++ De-Exchange/Bat.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:minutes
+
+pragma solidity ^0.8.0;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Bat is ERC20 {
+    
+    constructor() ERC20("BAT", "Brave Browser Token") {}
+    
+}
